a657876a04250e4d3ef86f6dc6155870c8b7aaec
Justin Turner <jmturner638@gmail.com>
2021-08-01 06:55:33 -0500

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,12 @@
 def main():
     #write your code below this line
+    pass
+
+def sum_list(numbers):
+    result = 0
+    for number in numbers:
+        result += number
+    return result
 
 if __name__ == '__main__':
     main()
